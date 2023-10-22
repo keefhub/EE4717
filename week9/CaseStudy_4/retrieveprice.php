9ef1ff42e36183acf99d688cca6d555791b660a1
keef <limy0313@e.ntu.edu.sg>
2023-10-22 20:25:09 +0800

    case study 4 update

---
+++ week9/CaseStudy_4/retrieveprice.php
@@ -0,0 +1,35 @@
+<?php
+// Perform the necessary database or server-side operations to update the price
+// Example: Update the price in a database
+// Replace these lines with your database logic
+$dbHost = "localhost";
+$dbUser = "admin";
+$dbPass = "javajam";
+$dbName = "javajam";
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query to fetch coffee prices
+$sql = "SELECT ID, price FROM coffeeprice";
+
+$result = $conn->query($sql);
+
+// Create an array to store the results
+$prices = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $prices[] = $row;
+    }
+}
+// Close the database connection
+$conn->close();
+
+// Send the prices as JSON
+header('Content-Type: application/json');
+echo json_encode($prices);
+?>
